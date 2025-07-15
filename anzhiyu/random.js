5f1a656400c01af8dc7fdc7b9f5edb4548a66648
xiaoyuan-c <chen020109xy@163.com>
2025-07-15 11:47:18 +0800

    Site updated: 2025-07-15 11:47:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/13/Getting-Started-for-hexo/","2025/07/14/hello-world/"];function toRandomPost(){
+var posts=["2025/07/14/hello-world/","2025/07/13/Getting-Started-for-hexo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
